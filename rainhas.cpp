e38d9a4ae4cb835b62287ae79499d3ead10c43c0
matheusgit <matheusdesouzafernandes54@gmail.com>
2024-05-20 15:17:52 -0300

    Nono commit

--- rainhas.cpp
+++ rainhas.cpp
@@ -2,15 +2,19 @@
 
 #include <iostream>
 #include <fstream>
-
-using namespace std;
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cstdio>
+using std::cout; using std::cin;
+using std::endl; using std::string;
+using std::vector;
 
 /*Função VerificaTabuleiro verifica se o tabuleiro é válido, retornando -11 se o arquivo tem problema, -1 se é um tabuleiro inválido e 0 se é válido*/
-int VerificaTabuleiro() {
-    ifstream myinput;
-    myinput.open("myinput", ios::in) 
-    if (!myinput) {
+int VerificaTabuleiro(const std::string& myinput) {
+    std::ifstream arquivo(myinput);
+
+    if (!arquivo) {
         return -11;
     }
-
-}
+    
