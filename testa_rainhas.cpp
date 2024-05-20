2aa3a7af2fa7eee268c9e4c358248bf931ecc33e
matheusgit <matheusdesouzafernandes54@gmail.com>
2024-05-19 22:48:50 -0300

    Sétimo commit

---
+++ testa_rainhas.cpp
@@ -0,0 +1,64 @@
+/**
+*@file testa_rainhas.cpp
+*/
+
+#define CATCH_CONFIG_MAIN
+#include "rainhas.hpp"
+#include "catch.hpp"
+
+/**
+* TEST_CASE - casos sem solução
+* verifica_solucao_8_rainhas será a função que responderá os requisitos do problema             
+*/
+TEST_CASE("solucoes invalidas para o problema", "[rainhas]") {
+    REQUIRE(verifica_solucao_8_rainhas("teste_8_rainhas.txt") == 0);
+    REQUIRE(verifica_solucao_8_rainhas("tabuleiros/teste_13.txt") == 0);
+    REQUIRE(verifica_solucao_8_rainhas("tabuleiros/teste_14.txt") == 0);
+    REQUIRE(verifica_solucao_8_rainhas("tabuleiros/teste_15.txt") == 0);
+
+    REQUIRE(verifica_solucao_8_rainhas("tabuleiros/teste_1.txt") == 0);
+    REQUIRE(verifica_solucao_8_rainhas("tabuleiros/teste_2.txt") == 0);
+    REQUIRE(verifica_solucao_8_rainhas("tabuleiros/teste_3.txt") == 0);
+    REQUIRE(verifica_solucao_8_rainhas("tabuleiros/teste_4.txt") == 0);
+    REQUIRE(verifica_solucao_8_rainhas("tabuleiros/teste_5.txt") == 0);
+    REQUIRE(verifica_solucao_8_rainhas("tabuleiros/teste_6.txt") == 0);
+    REQUIRE(verifica_solucao_8_rainhas("tabuleiros/teste_7.txt") == 0);
+    REQUIRE(verifica_solucao_8_rainhas("tabuleiros/teste_8.txt") == 0);
+    REQUIRE(verifica_solucao_8_rainhas("tabuleiros/teste_9.txt") == 0);
+    REQUIRE(verifica_solucao_8_rainhas("tabuleiros/teste_10.txt") == 0);
+    REQUIRE(verifica_solucao_8_rainhas("tabuleiros/teste_30.txt") == 0);
+    REQUIRE(verifica_solucao_8_rainhas("tabuleiros/teste_31.txt") == 0);
+}
+
+/**
+* TEST_CASE - casos com solução
+* 
+*/
+
+TEST_CASE("solucoes validas para o problema", "[rainhas]") {
+    REQUIRE(verifica_solucao_8_rainhas("tabuleiros/teste_11.txt") == 1);
+
+    REQUIRE(verifica_solucao_8_rainhas("tabuleiros/teste_25.txt") == 1);
+    REQUIRE(verifica_solucao_8_rainhas("tabuleiros/teste_26.txt") == 1);
+    REQUIRE(verifica_solucao_8_rainhas("tabuleiros/teste_27.txt") == 1);
+    REQUIRE(verifica_solucao_8_rainhas("tabuleiros/teste_28.txt") == 1);
+    REQUIRE(verifica_solucao_8_rainhas("tabuleiros/teste_28.txt") == 1);
+}
+
+/**
+* TEST_CASE - casos com invalidez
+* 
+*/
+TEST_CASE("matriz invalida para o problema", "[rainhas]") {
+    REQUIRE(verifica_solucao_8_rainhas("tabuleiros/teste_12.txt") == -1);
+    REQUIRE(verifica_solucao_8_rainhas("tabuleiros/teste_16.txt") == -1);
+
+    REQUIRE(verifica_solucao_8_rainhas("tabuleiros/teste_17.txt") == -1);
+    REQUIRE(verifica_solucao_8_rainhas("tabuleiros/teste_18.txt") == -1);
+    REQUIRE(verifica_solucao_8_rainhas("tabuleiros/teste_19.txt") == -1);
+    REQUIRE(verifica_solucao_8_rainhas("tabuleiros/teste_20.txt") == -1);
+    REQUIRE(verifica_solucao_8_rainhas("tabuleiros/teste_21.txt") == -1);
+    REQUIRE(verifica_solucao_8_rainhas("tabuleiros/teste_22.txt") == -1);
+    REQUIRE(verifica_solucao_8_rainhas("tabuleiros/teste_23.txt") == -1);
+    REQUIRE(verifica_solucao_8_rainhas("tabuleiros/teste_24.txt") == -1);
+}
