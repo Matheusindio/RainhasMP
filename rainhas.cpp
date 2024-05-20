fbbac5aa054cfe343f730d6c14d761655a228b76
matheusgit <matheusdesouzafernandes54@gmail.com>
2024-05-20 12:04:56 -0300

    Oitavo commit

--- rainhas.cpp
+++ rainhas.cpp
@@ -1,1 +1,16 @@
 /*Aqui o código principal*/
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+/*Função VerificaTabuleiro verifica se o tabuleiro é válido, retornando -11 se o arquivo tem problema, -1 se é um tabuleiro inválido e 0 se é válido*/
+int VerificaTabuleiro() {
+    ifstream myinput;
+    myinput.open("myinput", ios::in) 
+    if (!myinput) {
+        return -11;
+    }
+
+}
