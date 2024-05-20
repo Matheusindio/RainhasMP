9a4b6974e0fb7a3582e9aacee3d16b8fbdf6088f
matheusgit <matheusdesouzafernandes54@gmail.com>
2024-05-20 15:50:44 -0300

    12 commit

--- rainhas.hpp
+++ rainhas.hpp
@@ -1,1 +1,14 @@
-/*Aqui rainhas.hpp*/
+#ifndef RAINHAS_HPP_
+#define RAINHAS_HPP_
+
+#include <string>
+
+/**
+*verifica_solucao retorna 1 caso seja uma solução para o problema,
+0 caso não seja uma solução para o problema,
+-1 caso a entradsa não seja válida
+filename vai corresponder ao arquivo .txt que tem os tabuleiros
+*/
+int verifica_solucao(const std::string& filename);
+
+#endif  // RAINHAS_HPP_
